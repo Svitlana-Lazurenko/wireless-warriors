4c22f5b98812a0560109bae8e1be5d200cf5a0e6
Artem <artembariga5@gmail.com>
2023-05-04 12:25:52 +0300

    fix

--- src/js/components/mobile-menu.js
+++
@@ -1,8 +0,0 @@
-const menuButton = document.querySelector('.header__menu');
-const mobileMenu = document.querySelector('.mobileMenu');
-
-menuButton.addEventListener('click', openMobileMenu);
-
-function openMobileMenu () {
-    mobileMenu.classList.add('openModal');
-}
