8e23436ba1767202cc7f7d6eff8bdbc1f0911e95
Volodymyr Horpynchenko <115591313+blowmybow@users.noreply.github.com>
2023-05-08 21:37:25 +0300

    update

--- src/js/fetching and rendering/films-by-search.js
+++ src/js/fetching and rendering/films-by-search.js
@@ -1,10 +1,11 @@
-import { BASE_THEMOVIEDB_URL, apiKey } from '../tmdb-api';
 import axios from 'axios';
+
+import { BASE_THEMOVIEDB_URL, apiKey } from '../tmdb-api';
 import { makeStarsMarkup } from '../components/star-markup';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchThemoviedbGenres } from '../fetching and rendering/film-genres';
 import { pagination } from '../pagination';
 
-class PostApiService {
+export default class PostApiService {
   constructor() {
     this.searchQuery = '';
     this.page = 1;
@@ -17,19 +18,11 @@ class PostApiService {
       query: this.searchQuery,
       page: this.page,
     });
-
-    try {
-      const response = await axios.get(
-        `${BASE_THEMOVIEDB_URL}/search/movie?${OPTIONS.toString()}`
-      );
-      if (response.status !== 200) {
-        throw new Error(response.status);
-      }
-      // this.incrementPage();
-      return response.data;
-    } catch (error) {
-      console.error(error.toJSON());
-    }
+    const { data } = await axios.get(
+      `${BASE_THEMOVIEDB_URL}/search/movie?${OPTIONS.toString()}`
+    );
+    this.incrementPage();
+    return data;
   }
   get query() {
     return this.searchQuery;
@@ -37,6 +30,19 @@ class PostApiService {
   set query(newQuery) {
     this.searchQuery = newQuery;
   }
+  get pageCurrent() {
+    return this.page;
+  }
+  set pageCurrent(newPageCurrent) {
+    this.page = newPageCurrent;
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+  decrementPage() {
+    this.page -= 1;
+  }
+
   resetPage() {
     this.page = 1;
   }
@@ -44,8 +50,8 @@ class PostApiService {
 
 const refs = {
   form: document.getElementById('search-form'),
+  pagination: document.getElementById('pagination'),
   gallery: document.querySelector('.gallery__films'),
-  pagination: document.querySelector('.tui-pagination'),
 };
 const postApiService = new PostApiService();
 
@@ -57,23 +63,16 @@ function onSearch(e) {
   postApiService.query = e.target.searchQuery.value.trim();
   postApiService.resetPage();
   clearCardList();
-  fetchPost();
+  fetchResultsFilms();
   refs.form.reset();
 }
-function fetchPost() {
-  postApiService
-    .fetchPost()
-    .then(data => {
-      renderMarkup(data.results);
-      // pagination(data.page, data.total_pages);
-    })
-    .catch(onError);
-}
 
-function createMarkup(data) {
-  return data.reduce(
-    (acc, { poster_path, title, genre_ids, release_date, vote_average }) => {
-      acc += `<li class="movie__card">
+function createMarkup(
+  { poster_path, title, genre_ids, release_date, vote_average },
+  genresList
+) {
+  const genreNames = getGenresName(genre_ids, genresList);
+  return `<li class="movie__card">
       <div class="movie__card-thumb">
         <a href="#" class="movie__link">
             <img src='https://image.tmdb.org/t/p/original${poster_path}' alt='${title}' loading='lazy' class='movie__image' width='395' height='574'/>
@@ -81,68 +80,136 @@ function createMarkup(data) {
       </div>
           <div class="info overlay">
             <div class="info-thumb__text"><h2 class="info__title">${title}</h2>
-              <p class="info__genre">${getGenreName(
-                genre_ids
-              )}<span>|</span>${onlyYearFilter(release_date)}</p></div>
+              <p class="info__genre">${genreNames}<span>|</span>${onlyYearFilter(
+    release_date
+  )}</p></div>
               <div class="info-thumb__vote"><p class="info__vote">${makeStarsMarkup(
                 vote_average,
                 'catalog__rating-stars'
               )}</p></div>
                 
           </div>
-          </li> `;
-      return acc;
-    },
-    ''
-  );
+          </li>`;
 }
-
-function renderMarkup(data) {
-  refs.gallery.innerHTML = createMarkup(data);
+function renderMarkup(markup) {
+  if (markup !== undefined)
+    refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
+async function getGalleryMarkup() {
+  try {
+    const { results } = await postApiService.fetchPost();
+    const { genres } = await fetchThemoviedbGenres();
+    let genresList = {};
+    genres
+      .map(({ id, name }) => {
+        genresList[id] = name;
+      })
+      .join('');
+    if (results.length === 0) throw new Error('No Data!');
+    return results.reduce(
+      (markup, result) => markup + createMarkup(result, genresList),
+      ''
+    );
+  } catch (err) {
+    onError(err);
+  }
+}
+async function fetchResultsFilms() {
+  try {
+    const markup = await getGalleryMarkup();
+    renderMarkup(markup);
+    // pagination(markup.page, markup.total_pages, markup.total_results);
+  } catch (err) {
+    onError(err);
+  }
 }
 function onlyYearFilter(release_date) {
   return !release_date
     ? 'Unknown Year'
     : release_date.split('').slice(0, 4).join('');
 }
-function getGenreName(genre_ids) {
-  const genres = {
-    28: 'Action',
-    12: 'Adventure',
-    16: 'Animation',
-    35: 'Comedy',
-    80: 'Crime',
-    99: 'Documentary',
-    18: 'Drama',
-    10751: 'Family',
-    14: 'Fantasy',
-    36: 'History',
-    27: 'Horror',
-    10402: 'Music',
-    9648: 'Mystery',
-    10749: 'Romance',
-    878: 'Science Fiction',
-    10770: 'TV Movie',
-    53: 'Thriller',
-    10752: 'War',
-    37: 'Western',
-  };
-
-  if (genre_ids.length < 1) {
-    return genre_ids.map(elem => genres[elem]).join('');
+function getGenresName(genre_ids, genresList) {
+  try {
+    const genreIds = genre_ids.map(id => genresList[id]).join(' , ');
+    return genreIds;
+  } catch (error) {
+    console.error(error);
   }
-  return genre_ids
-    .map(elem => genres[elem])
-    .slice(0, 2)
-    .join(', ');
 }
 function onError(err) {
   console.error(err);
   clearCardList();
-  Notify.failure(
-    `❌ Sorry, there are no films matching your search query. Please try again.`
+  renderMarkup(
+    '<h2 class="title-error">OOPS...<br> We are very sorry!<br> We don’t have any results due to your search.</h2>'
   );
+  // Notify.failure(
+  //   `❌ Sorry, there are no films matching your search query. Please try again.`
+  // );
 }
 function clearCardList() {
   refs.gallery.innerHTML = '';
 }
+// function onBtnPagination(e) {
+//   const { target, currentTarget } = e;
+
+//   if (target === currentTarget || target.textContent === '') {
+//     return;
+//   }
+
+//   if (target.textContent === '🡺') {
+//     postApiService.incrementPage();
+//     fetchResultsFilms();
+//     return;
+//   }
+
+//   if (target.textContent === '🡸') {
+//     postApiService.decrementPage();
+//     fetchResultsFilms();
+//     return;
+//   }
+
+//   postApiService.pageCurrent = Number(target.textContent);
+//   fetchResultsFilms(postApiService.pageCurrent);
+// }
+// function pagination(page, pages) {
+//   let markup = '';
+//   postApiService.pageCurrent = page;
+//   const beforeTwoPage = page - 2;
+//   const beforeOnePage = page - 1;
+//   const afterTwoPage = page + 2;
+//   const afterOnePage = page + 1;
+
+//   // &#129144; <
+//   // &#129146; >
+
+//   if (page > 1) {
+//     markup +=
+//       '<li class="pagination__item pagination__item--arrow">&#129144</li>';
+//     markup += '<li class="pagination__item">1</li>';
+//   }
+//   if (page > 4) {
+//     markup += '<li class="pagination__item pagination__item--dots">...</li>';
+//   }
+//   if (page > 3) {
+//     markup += `<li class="pagination__item">${beforeTwoPage}</li>`;
+//   }
+//   if (page > 2) {
+//     markup += `<li class="pagination__item">${beforeOnePage}</li>`;
+//   }
+//   markup += `<li class="pagination__item pagination__item--current-page">${postApiService.pageCurrent}</li>`;
+//   if (pages - 1 > postApiService.pageCurrent) {
+//     markup += `<li class="pagination__item">${afterOnePage}</li>`;
+//   }
+//   if (pages - 2 > postApiService.pageCurrent) {
+//     markup += `<li class="pagination__item">${afterTwoPage}</li>`;
+//   }
+//   if (pages - 3 > postApiService.pageCurrent) {
+//     markup += '<li class="pagination__item pagination__item--dots">...</li>';
+//   }
+//   if (pages > postApiService.pageCurrent) {
+//     markup += `<li class="pagination__item">${pages}</li>`;
+//     markup += `<li class="pagination__item pagination__item--arrow">&#129146</li>`;
+//   }
+
+//   refs.pagination.innerHTML = markup;
+// }
