3c4b3406d1bfa01fcee1be352442f7119be0370f
ruudyaa <martarudnytska26@gmail.com>
2023-05-09 16:09:01 +0400

    Update film-genres.js

--- src/js/fetching and rendering/film-genres.js
+++ src/js/fetching and rendering/film-genres.js
@@ -1,33 +1,35 @@
-import { BASE_THEMOVIEDB_URL, apiKey } from '../tmdb-api';
-import { ItcCustomSelect } from '../components/castom-select';
-import axios from 'axios';
+// import { BASE_THEMOVIEDB_URL, apiKey } from '../tmdb-api';
+// import { ItcCustomSelect } from '../components/castom-select';
+// import axios from 'axios';
 
-async function fetchThemoviedbGenres() {
-  const response = await axios(
-    `${BASE_THEMOVIEDB_URL}/genre/movie/list?api_key=${apiKey}&language=en-US`
-  );
-  const newCollection = await response.data;
+// async function fetchThemoviedbGenres() {
+//   const response = await axios(
+//     `${BASE_THEMOVIEDB_URL}/genre/movie/list?api_key=${apiKey}&language=en-US`
+//   );
+//   const newCollection = await response.data;
 
-  return newCollection;
-}
+//   return newCollection;
+// }
 
-export { fetchThemoviedbGenres };
+// export { fetchThemoviedbGenres };
 
-async function selectGenresAndMarkup() {
-  try {
-    const promis = await fetchThemoviedbGenres();
-    const data = promis.genres
-      .map((data, index) => {
-        return `<li class="itc-select__option" data-select="option" data-value="${data.name}" data-index="${index}">${data.name}</li>`;
-      })
-      .join('');
+// async function selectGenresAndMarkup() {
+//   try {
+//     const promis = await fetchThemoviedbGenres();
+//     const data = promis.genres
+//       .map((data, index) => {
+//         return `<li class="itc-select__option" data-select="option" data-value="${data.name}" data-index="${index}">${data.name}</li>`;
+//       })
+//       .join('');
 
-    document.querySelector('.itc-select__genres').insertAdjacentHTML('beforeend', data);
-    new ItcCustomSelect('#select-2');
-    document.querySelector('.itc-select__toggle').disabled = false;
-  } catch (error) {
-    console.error(error);
-  }
-}
+//     document
+//       .querySelector('.itc-select__genres')
+//       .insertAdjacentHTML('beforeend', data);
+//     new ItcCustomSelect('#select-2');
+//     document.querySelector('.itc-select__toggle').disabled = false;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
-selectGenresAndMarkup();
+// selectGenresAndMarkup();
