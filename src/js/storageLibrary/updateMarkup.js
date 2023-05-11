b844e4ea2eb24054bbf9c1105bb1742cc0a1f966
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2023-05-11 08:32:08 +0300

    add genges

--- src/js/storageLibrary/updateMarkup.js
+++ src/js/storageLibrary/updateMarkup.js
@@ -1,58 +1,74 @@
 import { arrayMyFilms } from './upadateStorage';
 import { makeStarsMarkup } from '../components/star-markup';
+import { fetchThemoviedbGenres } from '..//fetching and rendering/film-genres';
 
 if (document.location.href.includes('my-library.html')) {
-    const filmList = document.querySelector('.libraryFilms');
-const baseUrl = 'https://image.tmdb.org/t/p/w500/';
+  const filmList = document.querySelector('.libraryFilms');
+  const baseUrl = 'https://image.tmdb.org/t/p/w500/';
 
-let genresList = {};
+  async function loadMoviesWeek() {
+    try {
+      const { genres } = await fetchThemoviedbGenres();
 
-const markup = arrayMyFilms.reduce(
-    (markup, arrayMyFilms) => markup + createMarkup(arrayMyFilms, genresList),
-    ''
-);
+      let genresList = {};
 
-updateMoviesList(markup);
+      genres
+        .map(({ id, name }) => {
+          genresList[id] = name;
+        })
+        .join('');
 
-function createMarkup(
-    { ID, img, data, nameFilm, rating, genres }
-    
-  ) {
-    // const genreNames = getGenresName(genres, genresList);
+      console.log(genresList);
+
+      const markup = arrayMyFilms.reduce(
+        (markup, arrayMyFilms) => markup + createMarkup(arrayMyFilms, genresList),
+        ''
+      );
+      updateMoviesList(markup);
+    } catch (error) {
+      onFetchError(error);
+    }
+  }
+
+  function createMarkup({ ID, img, data, nameFilm, rating, genres }, genresList) {
+    const genreNames = getGenresName(genres, genresList);
+    console.log(genreNames);
+
     return `
      <li class='movie__card'>
      <div class='movie__link' data-id=${ID}>
       <img src='${baseUrl}${img}' alt='${nameFilm}' loading='lazy' class='movie__image' width='395' height='574'/>
         <h2 class='info-title'>${nameFilm}</h2>
-        <p class='info-genre'>${'заглушка'}<span> | </span>${onlyYearFilter(
-            data
-    )}</p>
+        <p class='info-genre'>${genreNames}<span> | </span>${onlyYearFilter(data)}</p>
         <p class='info-vote'>${makeStarsMarkup(
-            rating,
-          'hero__rating-stars'
+          rating,
+          'catalog__rating-stars'
+          // 'hero__rating-stars'
         )}</p>
       </div>
     </li>`;
-}
+  }
 
-function getGenresName(genre_ids, genresList) {
+  function getGenresName(genres, genresList) {
     try {
-      const genreId = genre_ids.map(id => genresList[id]).join(' , ');
+      const genreId = genres
+        .slice(0, 2)
+        .map(id => genresList[id])
+        .join(', ');
+
       return genreId;
     } catch (error) {
       console.error(error);
     }
-}
+  }
 
-function onlyYearFilter(release_date) {
-    return !release_date
-      ? 'Unknown Year'
-      : release_date.split('').slice(0, 4).join('');
-}
+  function onlyYearFilter(release_date) {
+    return !release_date ? 'Unknown Year' : release_date.split('').slice(0, 4).join('');
+  }
 
-function updateMoviesList(markup = '') {
+  function updateMoviesList(markup = '') {
     if (markup !== undefined) {
-        filmList.innerHTML = markup;
+      filmList.innerHTML = markup;
     }
-}
+  }
 }
