fbb046106292c9195673f3e955048b056d6f202d
Svitlana-Lazurenko <svitsvit222@gmail.com>
2023-05-11 10:58:55 +0300

    Update pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,6 +1,5 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
-import axios from 'axios';
 import { makeStarsMarkup } from './components/star-markup';
 // import { fetchThemoviedbGenres } from './fetching and rendering/film-genres';
 
@@ -9,121 +8,146 @@ const form = document.querySelector('.catalog__form');
 const pagination = document.querySelector('#pagination');
 form.addEventListener('submit', onSearchDefault);
 // form.addEventListener('submit', onSearchExtensions);
-const instance = new Pagination(pagination, {
-  totalItems: 80,
-  itemsPerPage: 4,
-  visiblePages: 3,
-  page: 1,
-});
+let totalItemsTest = 80;
+const genresTest = [{ id: 1, name: 'Comedy' }];
 let name = '';
 // let genre = '';
 // let country = '';
 // let year = '';
 
-async function fetchThemoviedbGenres() {
-  const response = await axios(
-    `https://api.themoviedb.org/3//genre/movie/list?api_key=df4f25ddce476816dc7867d9ac4bd1ea&language=en-US`
-  );
-  const newCollection = await response.data;
-  console.log(newCollection);
-  return newCollection;
-}
+// ===============================FETCH===============================================
 
-async function fetchThemoviedbWeek(page) {
-  const response = await axios(
+const fetchGenres = async () => {
+  const response = await fetch(
+    'https://api.themoviedb.org/3//genre/movie/list?api_key=df4f25ddce476816dc7867d9ac4bd1ea&language=en-US'
+  );
+  const genres = await response.json();
+  return genres;
+};
+
+const fetchMovies = async page => {
+  const response = await fetch(
     `https://api.themoviedb.org/3/trending/movie/week?api_key=df4f25ddce476816dc7867d9ac4bd1ea&page=${page}&language=en-US`
   );
-  const newCollection = await response.data;
-  return newCollection;
-}
+  const movies = await response.json();
+  return movies;
+};
 
-async function fetchThemoviedbName(page, name) {
-  const response = await axios(
-    `https://api.themoviedb.org/3/search/movie?api_key=df4f25ddce476816dc7867d9ac4bd1ea&page=${page}&language=en-US&query=${name}`
+const fetchMoviesByName = async (page, name) => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=df4f25ddce476816dc7867d9ac4bd1ea&page=${page}&query=${name}&language=en-US`
   );
-  const newCollection = await response.data;
-  return newCollection;
-}
+  const moviesByName = await response.json();
+  return moviesByName;
+};
 
-// ---------------взнати назви параметрів з документації АПІ та передати їх сюди-------------------
-// async function fetchThemoviedbParams(page, genre, country, year) {
-//   const response = await axios(
-//     `https://api.themoviedb.org/3/search/movie?api_key=df4f25ddce476816dc7867d9ac4bd1ea&page=${page}&language=en-US`
+// ---------------взнати назви параметрів для розширеого пошуку з документації АПІ та передати їх сюди-------------------
+// const fetchMoviesByNameAdvanced = async (page, name, genre, country, year) => {
+//   const response = await fetch(
+//     `https://api.themoviedb.org/3/search/movie?api_key=df4f25ddce476816dc7867d9ac4bd1ea&page=${page}&query=${name}&language=en-US`
 //   );
-//   const newCollection = await response.data;
-//   return newCollection;
-// }
+//   const moviesByNameAvanced = await response.json();
+//   return moviesByNameAdvanced;
+// };
 
-fetchThemoviedbGenres();
-// .then(data => {
-//   return data.json;
-// })
-// .then(data => {
-//   console.log(data);
-//   genres === data;
-// })
-// .catch(error => {
-//   console.error(error);
-// });
+// ===============================ON PAGE LOAD AND ON SEARCH================================
 
-const genres = [{ id: 1, name: 'Comedy' }];
-
-renderThemoviedbWeek(1, genres);
+// renderMovies(1, genresTest);
+fetchGenres().then(data => console.log(data));
 
 function onSearchDefault(event) {
   event.preventDefault();
   instance.reset();
   currentPage = 1;
+
   const {
     elements: { searchQuery },
   } = event.currentTarget;
   name = searchQuery.value.trim();
-  renderThemoviedbName(1, name);
-  // form.reset();
+  renderMoviesByName(1, name, genresTest);
+  form.reset();
 }
 
-function renderThemoviedbWeek(page, genresOfFilms) {
-  const films = fetchThemoviedbWeek(page);
-  const markup = createMarkup(films, genresOfFilms);
-  listOfCards.insertAdjacentHTML('beforeend', markup);
-}
-
-function renderThemoviedbName(page, name) {
-  console.log(fetchThemoviedbName(page, name));
-}
-
-// function onSearchExtensions(event) {
+// function onSearchAdvanced(event) {
 //   event.preventDefault();
+//   instance.reset();
+//   currentPage = 1;
+
 //   const {
 // ---------------взнати значення name кожного селекту форми та передати їх сюди-------------------
 //     elements: { searchQuery },
 //   } = event.currentTarget;
 //   name = searchQuery.value.trim();
-//   console.log(fetchThemoviedbName(1, name));
+//   genre = ;
+//   country = ;
+//   year = ;
+//   console.log(fetchMoviesByNameAdvanced(1, name, genre, country, year));
+//   renderMoviesByNameAdvanced(1, name, genre, country, year, genres);
 //   form.reset();
 // }
 
+const instance = new Pagination(pagination, {
+  totalItems: totalItemsTest,
+  itemsPerPage: 20,
+  visiblePages: 3,
+  page: 1,
+});
+
+// ==================================RENDERING==============================================
+
+function renderMovies(pageOfFilms, genresOfFilms) {
+  const films = fetchMovies(pageOfFilms);
+  totalItemsFilms = 80;
+  const markup = createMarkup(films, genresOfFilms);
+  console.log(fetchMovies(pageOfFilms));
+  listOfCards.insertAdjacentHTML('beforeend', markup);
+}
+
+function renderMoviesByName(pageOfFilms, nameOfFilms, genresOfFilms) {
+  const films = fetchMoviesByName(pageOfFilms, nameOfFilms);
+  totalItemsFilms = 80;
+  const markup = createMarkup(films, genresOfFilms);
+  console.log(fetchMoviesByName(pageOfFilms, nameOfFilms));
+  listOfCards.insertAdjacentHTML('beforeend', markup);
+}
+
+// function renderMoviesByNameAdvanced(page, name, genre, country, year, genresOfFilms) {
+//   totalItemsFilms = 80;
+//   console.log(fetchMoviesbyNameAdvanced(page, name, genre, country, year));
+// }
+
+// ================================TUI PAGINATION============================================
+
 instance.on('beforeMove', function (eventData) {
   if (name === '') {
-    console.log(instance.getCurrentPage());
-    renderThemoviedbWeek(eventData.page);
-    return confirm('Go to page ' + eventData.page + '? week');
+    renderMovies(eventData.page, genresTest);
+    return confirm('Go to page ' + eventData.page + '? movies');
   }
 });
 
 instance.on('beforeMove', function (eventData) {
   if (name !== '') {
-    console.log(instance.getCurrentPage());
-    renderThemoviedbName(eventData.page, name);
-    return confirm('Go to page ' + eventData.page + '? name');
+    renderMoviesByName(eventData.page, name, genresTest);
+    return confirm('Go to page ' + eventData.page + '? by name');
   }
 });
 
+// instance.on('beforeMove', function (eventData) {
+// --------------Написати розширену перевірку-------------------
+//   if (name !== '') {
+//     console.log(instance.getCurrentPage());
+//     renderMoviesByNameAdvanced(eventData.page, name, genre, country, year, genres);
+//     return confirm('Go to page ' + eventData.page + '? by name advanced');
+//   }
+// });
+
+// =========================================FOR RENDERING========================================
+
 function createMarkup(
   { id, poster_path, release_date, title, vote_average, genre_ids },
-  genresList
+  genresListOfFilms
 ) {
-  const genresNames = getGenresName(genresList, genre_ids);
+  const genresNames = getGenresName(genresListOfFilms, genre_ids);
   return `
    <li class='movie__card'>
    <div class='movie__link' data-id=${id}>
@@ -141,7 +165,6 @@ function createMarkup(
 }
 
 function getGenresName(genresList, ids) {
-  console.log(genresList);
   try {
     let arrayOfGenres = [];
     for (const object of genresList) {
@@ -151,10 +174,10 @@ function getGenresName(genresList, ids) {
     }
 
     const commonArrayOfGenres = arrayOfGenres.concat(ids);
-    const genres = commonArrayOfGenres.filter(
+    const uniqueGenres = commonArrayOfGenres.filter(
       (genre, index, array) => array.indexOf(genre) === index
     );
-    const genresString = genres.join(', ');
+    const genresString = uniqueGenres.join(', ');
     return genresString;
   } catch (error) {
     console.error(error);
@@ -166,43 +189,3 @@ function onlyYearFilter(release_date) {
     ? 'Unknown Year'
     : release_date.split('').slice(0, 4).join('');
 }
-
-// ===================================================
-// function getGenresName(genre_ids, genresList) {
-//   try {
-//     const genreIds = genre_ids.map(id => genresList[id]).join(' , ');
-//     return genreIds;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// instance.on('beforeMove', function (eventData) {
-//   console.log(fetchThemoviedbParams(eventData.page));
-//   return confirm('Go to page ' + eventData.page + '?');
-// });
-
-// function createMarkup {
-
-// }
-
-// function createMarkup(
-//   { id, poster_path, release_date, title, vote_average, genre_ids },
-//   genresList
-// ) {
-//   const genreNames = getGenresName(genre_ids, genresList);
-//   return `
-//    <li class='movie__card'>
-//    <div class='movie__link' data-id=${id}>
-//     <img src='${img}${poster_path}' alt='${title}' loading='lazy' class='movie__image' width='395' height='574'/>
-//       <h2 class='info-title'>${title}</h2>
-//       <p class='info-genre'>${genreNames}<span> | </span>${onlyYearFilter(
-//     release_date
-//   )}</p>
-//       <p class='info-vote'>${makeStarsMarkup(
-//         vote_average,
-//         'hero__rating-stars'
-//       )}</p>
-//     </div>
-//   </li>`;
-// }
